29c9a90658f29c753887d6ac4c1592f956775c5e
Ambrosius <ambrosius411@protonmail.com>
2023-02-21 16:53:16 +0100

    Add function to reset the grid

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
+const gridSize = 600;
 const mainContainer = document.querySelector('#mainContainer');
 console.log(mainContainer)
 
-
 function createGrid(numberOfSquares) {
 
-for (let i = 1; i<=numberOfSquares; i++ ){
-    const divRowContainer = document.createElement('div');
-    divRowContainer.id = `columnContainer${i}`;
-    divRowContainer.classList.add('column-container');
+    for (let i = 1; i <= numberOfSquares; i++) {
+        const divRowContainer = document.createElement('div');
+        divRowContainer.id = `columnContainer${i}`;
+        divRowContainer.classList.add('column-container');
 
-    for (let j = 1; j<=numberOfSquares; j++ ){
-        const div = document.createElement('div');
-        div.classList.add(`div-column-${i}`);
-        div.classList.add(`div-row-${j}`);
-        div.classList.add(`div-grid`);
-        divRowContainer.appendChild(div)
-    }
-    mainContainer.appendChild(divRowContainer)
+        for (let j = 1; j <= numberOfSquares; j++) {
+            const div = document.createElement('div');
+            div.classList.add(`div-column-${i}`);
+            div.classList.add(`div-row-${j}`);
+            div.classList.add(`div-grid`);
+            divRowContainer.appendChild(div)
+        }
+        mainContainer.appendChild(divRowContainer)
     }
     document.querySelectorAll('.div-grid').forEach(item => {
         item.addEventListener('mouseover', hoverEffect)
@@ -33,9 +33,17 @@ function hoverEffect(e) {
 
 function chooseNumberOfSquares() {
     let numberOfSquares = prompt("Please choose the amount of squares per side")
+    deleteGrid()
     createGrid(numberOfSquares)
 }
 
+function deleteGrid() {
+    document.querySelectorAll('.column-container').forEach(item => {
+        mainContainer.removeChild(item)
+
+    })
+}
+
 function determineSizePerSquare(numberOfSquares) {
-    return (600 / numberOfSquares)
+    return (gridSize / numberOfSquares)
 }
