d2385b34edf73b2b832fec135fbee12dc812129a
Ambrosius <ambrosius411@protonmail.com>
2023-02-22 16:41:31 +0100

    Add different colorshades to rainbow mode

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 const gridSize = 600;
+const defaultColorBackground = 'rgb(204,250,204)'
+const defaultColor = '#ffe5e9'
 let eraser = false;
 let mouseDown = false;
+let rainbowMode = false;
+let pixelsVisible = false;
+let colorIteration = 256; //Makes it so that the colors in rainbow mode get progressively darker
+let shadeColorIteration = 0 //Makes it so that in rainbow mode different color shades alternate. First blue, green, purple, red, orange
+
 const mainContainer = document.querySelector('#mainContainer');
 
 createGrid(20)
@@ -29,6 +36,7 @@ function createGrid(numberOfSquares) {
         let squareSize = determineSizePerSquare(numberOfSquares)
         item.style.height = `${squareSize}px`;
         item.style.width = `${squareSize}px`;
+        if (pixelsVisible) item.classList.add("solid-border");
     })
 }
 
@@ -36,11 +44,68 @@ function eraseMode(booleanValue){
     eraser = booleanValue
 }
 
-function changeTileColor(e) {
-    if (mouseDown && !eraser) e.target.classList.add('colored');
-    if (mouseDown && eraser) e.target.classList.remove('colored');
-    }
+function toggleRainbow(){
+    rainbowMode ? rainbowMode = false : rainbowMode = true;
+}
 
+function toggleBorderStyle() {
+    if (!pixelsVisible) {
+        document.querySelectorAll('.div-grid').forEach(item => {
+            item.classList.add("solid-border");
+        })
+        pixelsVisible = true;
+    }
+    else {
+        document.querySelectorAll('.div-grid').forEach(item => {
+            item.classList.remove("solid-border");
+        })
+        pixelsVisible = false;
+    }
+}
+
+function randomColor() {
+    var o = Math.round, r = Math.random, s = 50;
+    if (colorIteration < 25 ) {
+        colorIteration = 256; 
+        shadeColorIteration++;
+    }
+    colorIteration--
+    let color = returnColorShade(shadeColorIteration % 5, o,r,s);
+    console.log(color)
+    return color
+}
+
+function returnColorShade(shadeColorIteration, o,r,s) {
+    switch (shadeColorIteration) {
+        case 0:
+            return 'rgb(' + o((r()*s) + colorIteration + 100) + ',' + o((r()*s) + colorIteration -50) + ',' + o((r()*s) + colorIteration - 50) + ')'; //red shades
+        case 1:
+            return 'rgb(' + o((r()*s) + colorIteration + 100) + ',' + o((r()*s) + colorIteration) + ',' + o((r()*s) + colorIteration - 100) + ')'; //orange shades
+        case 2:
+            return 'rgb(' + o((r()*s) + colorIteration) + ',' + o((r()*s) + colorIteration + 50) + ',' + o((r()*s) + colorIteration - 50) + ')'; //green shades
+        case 3:
+            return 'rgb(' + o((r()*s) + colorIteration - 50) + ',' + o((r()*s) + colorIteration) + ',' + o((r()*s) + colorIteration + 50) + ')';  //blue shades
+        case 4:
+            return 'rgb(' + o((r()*s) + colorIteration + 25) + ',' + o((r()*s) + colorIteration - 25) + ',' + o((r()*s) + colorIteration + 75) + ')'; //purple shades
+    }
+}
+
+function changeTileColor(e) {
+    // if (mouseDown && !eraser) e.target.classList.add('colored')
+    // else if(mouseDown && eraser) e.target.classList.remove('colored');
+    if (!mouseDown) return;
+    if (eraser) {
+        e.target.style.backgroundColor = defaultColorBackground;
+    }
+    else if (rainbowMode) {
+        e.target.style.backgroundColor = randomColor()
+    }
+    else {
+        e.target.style.backgroundColor = defaultColor;
+    }
+}
+
+    
 function chooseNumberOfSquares() {
     let numberOfSquares = parseInt(prompt("Please choose the amount of squares per side"))
         
@@ -62,7 +127,9 @@ function deleteGrid() {
 
 function resetGrid() {
     document.querySelectorAll('.div-grid').forEach(item => {
-        item.classList.remove('colored')
+        item.style.backgroundColor = defaultColorBackground;
+        if (pixelsVisible) item.classList.add("solid-border");
+            
     })
 }
 
